0e53741af9132d2de0a4599051a932e70646d8ab
Nikolaj <rambo3889@gmail.com>
2019-09-25 22:06:01 +0200

    Fixed small indention issues

--- Database/Gear/Manufacturers.sql
+++ Database/Gear/Manufacturers.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [Gear].[Manufacturers]
 (
-    [Id]    INT                 NOT NULL PRIMARY KEY IDENTITY(1,1),
-    [Name]  NVARCHAR(256)       NOT NULL
+    [Id]    INT             NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [Name]  NVARCHAR(256)   NOT NULL
 )
--- Database/Jump/AdditionalJumpers.sql
+++ Database/Jump/AdditionalJumpers.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Jump].[AdditionalJumpers]
 (
-    [JumpId]        INT NOT NULL,
-    [PersonId]      INT NOT NULL,
+    [JumpId]    INT NOT NULL,
+    [PersonId]  INT NOT NULL,
     CONSTRAINT [PK_AdditionalJumper] PRIMARY KEY ([JumpId], [PersonId]),
     CONSTRAINT [FK_Jump.AdditionalJumpers.JumpId_Jump.Jumps.Id]         FOREIGN KEY ([JumpId])      REFERENCES [Jump].[Jumps]([Id]),
     CONSTRAINT [FK_Jump.AdditionalJumpers.PersonId_Person.Persons.Id]   FOREIGN KEY ([PersonId])    REFERENCES [Person].[Persons]([Id])
