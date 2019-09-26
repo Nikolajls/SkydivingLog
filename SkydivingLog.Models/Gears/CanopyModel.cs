3195c52cf0d14b654a8052aef8ee0d28a1d7507e
Unknown <niso@bws.dk>
2019-09-26 23:24:38 +0200

    Expanded the Canopy model a bit.

--- SkydivingLog.Models/Gears/CanopyModel.cs
+++ SkydivingLog.Models/Gears/CanopyModel.cs
@@ -8,5 +8,26 @@ namespace SkydivingLog.Models.Gears
         public int Id { get; set; }
         public int ManufacturerId { get; set; }
         public string Name { get; set; }
+        public int Cells { get; set; }
+        public bool Elliptical { get; set; }
+        public CanopyType Type { get; set; }
+        public CanopyLevel Level { get; set; }
+
+    }
+
+    public enum CanopyLevel
+    {
+        Student,
+        Novice,
+        Experienced,
+        VeryExperienced,
+        ExtremelyExperienced
+    }
+
+    public enum CanopyType
+    {
+        Main,
+        Reserve,
+        Foil
     }
 }
