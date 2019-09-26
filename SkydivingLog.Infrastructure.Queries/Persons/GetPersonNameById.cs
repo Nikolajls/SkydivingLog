837f7c15340154710f4a76c5a05c374526a9150d
Nikolaj <rambo3889@gmail.com>
2019-09-26 15:27:38 +0200

    Added prototype and queries projects
    Added mediatr,Dapper, sql and autofac to the projects and starting work on it

---
+++ SkydivingLog.Infrastructure.Queries/Persons/GetPersonNameById.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using MediatR;
+
+namespace SkydivingLog.Infrastructure.Queries.Persons
+{
+    public class GetPersonNameById
+    {
+        public class Query : IRequest<Result>
+        {
+            public int Id { get; set; }
+        }
+
+        public class Result
+        {
+            public string Name { get; set; }
+        }
+
+        public class QueryHandler : IRequestHandler<Query, Result>
+        {
+            private readonly IDbConnection _connection;
+
+            public QueryHandler(IDbConnection connection)
+            {
+                _connection = connection;
+            }
+
+            public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var data = await _connection.QueryFirstOrDefaultAsync<Result>("SELECT * FROM [Person].[Persons] WHERE Id = @Id", request);
+
+                return data;
+            }
+        }
+    }
+}
---
+++ SkydivingLog.Presentation.Prototype/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Features.Variance;
+using MediatR;
+using SkydivingLog.Infrastructure.Queries;
+using SkydivingLog.Infrastructure.Queries.Persons;
+
+namespace Prototype
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var container = SetupAutofac();
+            var mediatr = container.Resolve<IMediator>();
+            var result = await mediatr.Send(new GetPersonNameById.Query() { Id = 1 });
+            Console.WriteLine($"Person with Id 1 names({result?.Name})");
+            Console.WriteLine("Idling...");
+            Console.ReadLine();
+        }
+
+        private static IContainer SetupAutofac()
+        {
+            var builder = new ContainerBuilder();
+            RegisterMediator(builder);
+
+            builder.Register(context =>
+            {
+                var sqlConnection = new SqlConnection("Server=.;Integrated Security=true; Database=SkydivingLog; MultipleActiveResultSets=True;");
+                return sqlConnection;
+            }).InstancePerLifetimeScope();
+
+            builder.Register(context =>
+            {
+                var sqlConnection = new SqlConnection("Server=.;Integrated Security=true; Database=SkydivingLog; MultipleActiveResultSets=True;");
+                return sqlConnection;
+            }).As<IDbConnection>().InstancePerLifetimeScope();
+            var container = builder.Build();
+            return container;
+        }
+
+
+        private static void RegisterMediator(ContainerBuilder builder)
+        {
+            // Mediator itself
+            builder
+                .RegisterType<Mediator>()
+                .As<IMediator>()
+                .InstancePerLifetimeScope();
+
+            // Enables contravariant Resolve() for interfaces with single contravariant ("in") arg
+            builder
+                .RegisterSource(new ContravariantRegistrationSource());
+
+
+
+            // Request handlers
+            builder.RegisterAssemblyTypes(typeof(AssemblyAnchor).GetTypeInfo().Assembly).AsImplementedInterfaces(); // via assembly scan
+
+            // request & notification handlers
+            builder.Register<ServiceFactory>(context =>
+            {
+                var c = context.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+        }
+    }
+}
