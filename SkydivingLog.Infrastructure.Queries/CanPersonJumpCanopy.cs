2ed99b1edcb23b945f6b6000099e37aedfda5a92
Unknown <niso@bws.dk>
2019-09-26 23:28:31 +0200

    Just testing mediatr, working on calculating if a person may jump a canopy by DK rules

---
+++ SkydivingLog.Infrastructure.Queries/CanPersonJumpCanopy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SkydivingLog.Infrastructure.Queries
+{
+    public class CanPersonJumpCanopy
+    {
+        public class Query : IRequest<bool>
+        {
+            public int PersonId { get; set; }
+            
+            public int CanopyId { get; set; }
+
+            public int LeadWeightGrams { get; set; }
+        }
+
+        public class QueryHandler : IRequestHandler<Query, bool>
+        {
+            private readonly IDbConnection _connection;
+
+            public QueryHandler(IDbConnection connection)
+            {
+                _connection = connection;
+            }
+
+            public Task<bool> Handle(Query request, CancellationToken cancellationToken)
+            {
+                //Get jumper weight
+                //Downsizing no worries...
+                return Task.FromResult(true);
+            }
+        }
+    }
+}
