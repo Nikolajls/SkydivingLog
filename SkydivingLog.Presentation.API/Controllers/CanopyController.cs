5785c1cff34ef3d1a15c139e11cadf3338a7cbf9
Nikolaj <rambo3889@gmail.com>
2019-11-16 12:53:43 +0100

    Added API project start

---
+++ SkydivingLog.Presentation.API/Controllers/CanopyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkydivingLog.Models.Gears;
+using System.Collections.Generic;
+
+namespace SkydivingLog.Presentation.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CanopyController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = new List<Canopy>()
+            {
+                new Canopy() {SerialNumber = "12345"}
+            };
+            return Ok(data);
+        }
+    }
+}
